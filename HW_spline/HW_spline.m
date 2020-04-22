b9b30223811e73fb5d55455a5fdfae2e0ac7990e
Ali <alibaniasad1999@yahoo.com>
2020-04-22 18:24:19 +0430

    just fucking debug

--- HW_spline/HW_spline.m
+++ HW_spline/HW_spline.m
@@ -13,8 +13,8 @@ A = [
 0   2   0   0   0   0   0   0   0   0   0   0 ;
 0   0   0   0   0   0   0   0   6   2   0   0 
 ];
-B_x = [0;0;0;5;5;1;0;0;0;0;0;0];
-B_y = [0;2;2;7;7;-2;0;0;0;0;0;0];
+B_y = [0;0;0;5;5;1;0;0;0;0;0;0];
+B_x = [0;2;2;7;7;-2;0;0;0;0;0;0];
 answer_x  = (A^-1)*B_x;
 answer_y  = (A^-1)*B_y;
 u = 0:0.1:1;
@@ -26,8 +26,8 @@ C_2_y = answer_y(5).*u.^3+answer_y(6).*u.^2+answer_y(7).*u+answer_y(8);
 C_3_y = answer_y(9).*u.^3+answer_y(10).*u.^2+answer_y(11).*u+answer_y(12);
 X_spline = [C_1_x, C_2_x, C_3_x];
 Y_spline = [C_1_y, C_2_y, C_3_y];
-X = [0 0 5 1];
-Y = [0 2 7 -2];
+Y = [0 0 5 1];
+X = [0 2 7 -2];
 figure;
 plot(X_spline, Y_spline)
 hold
